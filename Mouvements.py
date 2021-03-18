c31af002463e45147edebb18812f5755c0544556
QuentinF0 <quentin.fourie000@gmail.com>
2021-03-18 11:07:28 +0100

    Fonction mouvement

--- Mouvements.py
+++ Mouvements.py
@@ -26,20 +26,8 @@ while continuer:
         if event.type == QUIT:
             continuer = 0
         if event.type == KEYDOWN:
-            if event.key == K_DOWN:  # Si "flèche bas"
-                # On descend le perso
-                position_perso = position_perso.move(0, 5)
-            if event.key == K_UP:  # Si "flèche haut"
-                # On descend le perso
-                position_perso = position_perso.move(0, -5)
-            if event.key == K_LEFT:  # Si "flèche gauche"
-                # On descend le perso
-                perso = pygame.image.load("perso.png").convert_alpha()
-                position_perso = position_perso.move(-5, 0)
-            if event.key == K_RIGHT:  # Si "flèche droite"
-                # On descend le perso
-                perso = pygame.image.load("ServietD.png").convert_alpha()
-                position_perso = position_perso.move(5, 0)
+            if event.type == KEYDOWN:
+                mouves(event.key)
     # Re-collage
     fenetre.blit(fond, (0, 0))
     fenetre.blit(perso, position_perso)
---
+++ Mouves.py
@@ -0,0 +1,23 @@
+"Création de la fonction mouvement"
+
+def mouves(key):
+    global position_perso, perso
+    if event.type == KEYDOWN:
+        if key == K_DOWN:  # Si "flèche bas"
+            # On descend le perso
+            position_perso = position_perso.move(0, 5)
+        if key == K_UP:  # Si "flèche haut"
+            # On monte le perso
+            position_perso = position_perso.move(0, -5)
+        if key == K_LEFT:  # Si "flèche gauche"
+            # On bouge à gauche le perso
+            perso = pygame.image.load("perso.png").convert_alpha()
+            position_perso = position_perso.move(-5, 0)
+            sprite("L")
+        if key == K_RIGHT:  # Si "flèche droite"
+            # On bouge à droite le perso
+            print("yes 1")
+            perso = pygame.image.load("ServietD.png").convert_alpha()
+            position_perso = position_perso.move(5, 0)
+            sprite("R")
+            print("yes 2")
