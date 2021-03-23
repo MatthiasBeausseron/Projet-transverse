798def5e4e88fba41008d8a8b321eadfd0b24988
QuentinF0 <quentin.fourie000@gmail.com>
2021-03-23 08:16:51 +0100

    Changement fonction Mouves

--- Mouves.py
+++ Mouves.py
@@ -11,13 +11,13 @@ def mouves(key):
             position_perso = position_perso.move(0, -5)
         if key == K_LEFT:  # Si "flèche gauche"
             # On bouge à gauche le perso
-            perso = pygame.image.load("perso.png").convert_alpha()
+            perso = pygame.image.load("photos/perso.png").convert_alpha()
             position_perso = position_perso.move(-5, 0)
             sprite("L")
         if key == K_RIGHT:  # Si "flèche droite"
             # On bouge à droite le perso
             print("yes 1")
-            perso = pygame.image.load("ServietD.png").convert_alpha()
+            perso = pygame.image.load("photos/ServietD.png").convert_alpha()
             position_perso = position_perso.move(5, 0)
             sprite("R")
             print("yes 2")
