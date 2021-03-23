efce1747c088bcc1b2434b9f31f40ad40d4e3d7c
QuentinF0 <quentin.fourie000@gmail.com>
2021-03-23 11:19:00 +0100

    Sprite right and left

--- sprite.py
+++ sprite.py
@@ -4,14 +4,21 @@ class MySprite(pygame.sprite.Sprite):
     def __init__(self):
         super(MySprite, self).__init__()
 
-        self.imagesR = []
-        self.imagesL = []
-        self.imagesR.append(pygame.image.load('images/IdleD0.png'))
-        self.imagesR.append(pygame.image.load('images/IdleD1.png'))
-        self.imagesR.append(pygame.image.load('images/IdleD2.png'))
-        self.imagesR.append(pygame.image.load('images/IdleD4.png'))
-        self.imagesR.append(pygame.image.load('images/IdleD4.png'))
-        self.imagesL.append(pygame.image.load('images/'))
+class MySprite(pygame.sprite.Sprite):
+    def __init__(self):
+        super(MySprite, self).__init__()
+
+        self.images = []
+        self.images.append(pygame.image.load('images/IdleD0.png'))
+        self.images.append(pygame.image.load('images/IdleD1.png'))
+        self.images.append(pygame.image.load('images/IdleD2.png'))
+        self.images.append(pygame.image.load('images/IdleD4.png'))
+        self.images.append(pygame.image.load('images/IdleD4.png'))
+        self.images.append(pygame.image.load('images/IdleG0.png'))
+        self.images.append(pygame.image.load('images/IdleG1.png'))
+        self.images.append(pygame.image.load('images/IdleG2.png'))
+        self.images.append(pygame.image.load('images/IdleG3.png'))
+        self.images.append(pygame.image.load('images/IdleG4.png'))
 
         self.index = 0
 
@@ -21,19 +28,7 @@ class MySprite(pygame.sprite.Sprite):
 
         self.rect = pygame.Rect(5, 5, 150, 198)
 
-    def updateR(self):
-        if self.cnt % 9 == 0:
-            self.index += 1
-
-        if self.index >= len(self.images):
-            self.index = 0
-
-        self.image = self.images[self.index]
-
-        self.cnt += 1
-    
-
-    def updateL(self):
+    def update(self):
         if self.cnt % 9 == 0:
             self.index += 1
 
