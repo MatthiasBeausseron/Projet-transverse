4bf0a83e75f5faf86dd2a8ff9124ae286edf1c6e
QuentinF0 <quentin.fourie000@gmail.com>
2021-04-12 08:55:32 +0200

    Animation of idle + walk

--- sprite.py
+++ sprite.py
@@ -19,9 +19,19 @@ class MySprite(pygame.sprite.Sprite):
         self.images.append(pygame.image.load('images/IdleG2.png'))
         self.images.append(pygame.image.load('images/IdleG3.png'))
         self.images.append(pygame.image.load('images/IdleG4.png'))
+         self.images.append(pygame.image.load('images/WalkServiet00.png'))
+        self.images.append(pygame.image.load('images/WalkServiet01.png'))
+        self.images.append(pygame.image.load('images/WalkServiet02.png'))
+        self.images.append(pygame.image.load('images/WalkServiet03.png'))
+        self.images.append(pygame.image.load('images/WalkServiet04.png'))
+        self.images.append(pygame.image.load('images/WalkServiet05.png'))
+        self.images.append(pygame.image.load('images/WalkServiet06.png'))
+        self.images.append(pygame.image.load('images/WalkServiet07.png'))
+        self.images.append(pygame.image.load('images/WalkServiet08.png'))
+        self.images.append(pygame.image.load('images/WalkServiet09.png'))
 
         self.index = 0
-
+        self.idle = 0
         self.cnt = 0
 
         self.image = self.images[self.index]
@@ -29,12 +39,24 @@ class MySprite(pygame.sprite.Sprite):
         self.rect = pygame.Rect(5, 5, 150, 198)
 
     def update(self):
+        keys = pygame.key.get_pressed()
         if self.cnt % 9 == 0:
             self.index += 1
 
         if self.index >= len(self.images):
             self.index = 0
 
-        self.image = self.images[self.index]
+        if self.idle == 0:
+            self.image = self.images[self.index % 5]
+
+        if self.idle == 1:
+            self.image = self.images[self.index % 5 + 5]
+
+        if keys[pygame.K_RIGHT]:
+            self.image = self.images[self.index % 5 + 10]
+            self.idle = 0
+        if keys[pygame.K_LEFT]:
+            self.image = self.images[self.index % 5 + 15]
+            self.idle = 1
 
         self.cnt += 1
